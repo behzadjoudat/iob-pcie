41f23c5334211f8f4982bb62e29a21a138166d50
Behzad Joudat <joudat.behzad@gmail.com>
2022-10-15 16:52:17 +0100

    pcie periph working with pcie pins through riffa
    the iob-pcie-core.v has been removed as no longer it is needed. and the necessary changes are made to iob-pcie.v and pio.vh

--- hardware/src/iob_pcie.v
+++ hardware/src/iob_pcie.v
@@ -5,7 +5,8 @@
 module iob_pcie
   # (
      parameter DATA_W = 32,
-     parameter ADDR_W = `iob_pcie_swreg_ADDR_W
+     parameter ADDR_W = `iob_pcie_swreg_ADDR_W,
+     parameter C_PCI_DATA_WIDTH = 64
      )
    (
 
@@ -21,7 +22,7 @@ module iob_pcie
     `IOB_INPUT(PCIE_CHNL_RX_LAST, 1),
     `IOB_INPUT(PCIE_CHNL_RX_LEN, 32),
     `IOB_INPUT(PCIE_CHNL_RX_OFF, 31),
-    `IOB_INPUT(PCIE_CHNL_RX_DATA, 32),
+    `IOB_INPUT(PCIE_CHNL_RX_DATA, 64),
     `IOB_INPUT(PCIE_CHNL_RX_DATA_VALID, 1),
     `IOB_OUTPUT(PCIE_CHNL_RX_DATA_REN, 1),
     `IOB_OUTPUT(PCIE_CHNL_TX_CLK, 1),
@@ -30,7 +31,7 @@ module iob_pcie
     `IOB_OUTPUT(PCIE_CHNL_TX_LAST, 1),
     `IOB_OUTPUT(PCIE_CHNL_TX_LEN, 32),
     `IOB_OUTPUT(PCIE_CHNL_TX_OFF, 31),
-    `IOB_OUTPUT(PCIE_CHNL_TX_DATA, 32),
+    `IOB_OUTPUT(PCIE_CHNL_TX_DATA, 64),
     `IOB_OUTPUT(PCIE_CHNL_TX_DATA_VALID, 1),
     `IOB_INPUT(PCIE_CHNL_TX_DATA_REN, 1),
 
@@ -39,49 +40,84 @@ module iob_pcie
     );
 
 `include "iob_pcie_swreg_gen.vh"
-
-
-   `IOB_WIRE(PCIE_DATA_IN, 32)
+   
+   
+   `IOB_WIRE(PCIE_TX_DATA, 32)
    iob_reg #(.DATA_W(32))
-   pcie_data_in (
-		  .clk        (clk),
-		  .arst       (rst),
-		  .rst        (rst),
-		  .en         (PCIE_DATA_IN_en),
-		  .data_in    (PCIE_DATA_IN_wdata),
-		  .data_out   (PCIE_DATA_IN)
-		  );
-
-
-
-iob_pcie_core iob_pcie_core0
-  (
-   .clk(clk),
-   .rst(rst),
-   .CLK(PCIE_CLK),
-   .RST(PCIE_RST),
-   .CHNL_RX_CLK(PCIE_CHNL_RX_CLK),
-   .CHNL_RX(PCIE_CHNL_RX),
-   .CHNL_RX_ACK(PCIE_CHNL_RX_ACK),
-   .CHNL_RX_LAST(PCIE_CHNL_RX_LAST),
-   .CHNL_RX_LEN(PCIE_CHNL_RX_LEN),
-   .CHNL_RX_OFF(PCIE_CHNL_RX_OFF),
-   .CHNL_RX_DATA(PCIE_CHNL_RX_DATA),
-   .CHNL_RX_DATA_VALID(PCIE_CHNL_RX_DATA_VALID),
-   .CHNL_RX_DATA_REN(PCIE_CHNL_RX_DATA_REN),
-   .CHNL_TX_CLK(PCIE_CHNL_TX_CLK),
-   .CHNL_TX(PCIE_CHNL_TX),
-   .CHNL_TX_ACK(PCIE_CHNL_TX_ACK),
-   .CHNL_TX_LAST(PCIE_CHNL_TX_LAST),
-   .CHNL_TX_LEN(PCIE_CHNL_TX_LEN),
-   .CHNL_TX_OFF(PCIE_CHNL_TX_OFF),
-   .CHNL_TX_DATA(PCIE_CHNL_TX_DATA),
-   .CHNL_TX_DATA_VALID(PCIE_CHNL_TX_DATA_VALID),
-   .CHNL_TX_DATA_REN(PCIE_CHNL_TX_DATA_REN),
-   .CORE_SETTER(PCIE_DATA_IN),
-   .CORE_GETTER(PCIE_DATA_OUT_rdata)
-
- );
-
+   pcie_tx_data (
+		 .clk        (clk),
+		 .arst       (rst),
+		 .rst        (rst),
+		 .en         (PCIE_TX_DATA_en),
+		 .data_in    (PCIE_TX_DATA_wdata),
+		 .data_out   (PCIE_TX_DATA)
+		 );
+   
+   
+   
+   reg [C_PCI_DATA_WIDTH-1:0] rData={C_PCI_DATA_WIDTH{1'b0}};
+   reg [C_PCI_DATA_WIDTH-1:0] tData={C_PCI_DATA_WIDTH{1'b0}};
+   reg [31:0] 		      rLen=0;
+   reg [31:0] 		      rCount=0;
+   reg [1:0] 		      rState=0;
+   
+   assign PCIE_CHNL_RX_CLK = PCIE_CLK;
+   assign PCIE_CHNL_RX_ACK = (rState == 2'd1);
+   assign PCIE_CHNL_RX_DATA_REN = (rState == 2'd1);
+   
+   assign PCIE_CHNL_TX_CLK = PCIE_CLK;
+   assign PCIE_CHNL_TX = (rState == 2'd3);
+   assign PCIE_CHNL_TX_LAST = 1'd1;
+   assign PCIE_CHNL_TX_LEN = rLen; // in words
+   assign PCIE_CHNL_TX_OFF = 0;
+   assign PCIE_CHNL_TX_DATA = tData;
+   assign PCIE_CHNL_TX_DATA_VALID = (rState == 2'd3);
+   
+   always @(posedge PCIE_CLK or posedge PCIE_RST) begin
+      if (PCIE_RST) begin
+	 rLen <= #1 0;
+	 rCount <= #1 0;
+	 rState <= #1 0;
+	 rData <= #1 0;
+      end
+      else begin
+	 case (rState)
+	   
+	   2'd0: begin // Wait for start of RX, save length
+	      if (PCIE_CHNL_RX) begin
+				rLen <= #1 PCIE_CHNL_RX_LEN;
+		 rCount <= #1 0;
+		 rState <= #1 2'd1;
+	      end
+	   end
+	   
+	   2'd1: begin // Wait for last data in RX, save value
+	      if (PCIE_CHNL_RX_DATA_VALID) begin
+		 rData <= #1 PCIE_CHNL_RX_DATA;
+		 PCIE_RX_DATA_rdata <= rData;
+		 rCount <= #1 rCount + (C_PCI_DATA_WIDTH/32);
+	      end
+	      if (rCount >= rLen)
+		rState <= #1 2'd2;
+	   end
+	   
+	   2'd2: begin // Prepare for TX
+	      rCount <= #1 (C_PCI_DATA_WIDTH/32);
+	      rState <= #1 2'd3;
+	   end
+	   
+	   2'd3: begin // Start TX with save length and data value
+	      if (PCIE_CHNL_TX_DATA_REN & PCIE_CHNL_TX_DATA_VALID) begin
+		 tData <= #1 {rCount + 10, rCount + 9, rCount + 8, rCount + 7, rCount + 6, rCount + 5, rCount + 4, rCount + 3, rCount + 2, rCount + 1};
+		 rCount <= #1 rCount + (C_PCI_DATA_WIDTH/32);
+		 if (rCount >= rLen)
+		   rState <= #1 2'd0;
+	      end
+		end
+	   
+	 endcase
+      end
+   end
+      
 endmodule
     
